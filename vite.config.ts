8705ba98fe3779b014ba2018e9be250846226d3c
XIN <anxanxx@gmail.com>
2026-06-05 15:34:18 +0800

    chore: initial project configuration and tooling setup

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import mdx from "fumadocs-mdx/vite";
+import { defineConfig } from "vite";
+import * as MdxConfig from "./source.config";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tanstackStart(), mdx(MdxConfig), tailwindcss(), react()],
+});
